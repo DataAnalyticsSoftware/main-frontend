71a0ca26daacd27f97a3a4a59132b6fb2be7c2df
Victor Ortega <victorortega0309@gmail.com>
2024-03-03 17:40:49 +0100

    bugfix
    cards responsive landing

--- src/pages/LandingPage/components/ContentComponent/components/WhyChooseUs/index.tsx
+++ src/pages/LandingPage/components/ContentComponent/components/WhyChooseUs/index.tsx
@@ -12,11 +12,11 @@ export const WhyChooseUs=()=> {
       <Typography variant="h4" sx={{paddingBottom:2, color:'black'}}>Why Choose<Typography sx={{color:'#fe5b05', pl:1}} variant="h4"> Us?</Typography></Typography>
       <Typography variant="subtitle1" paragraph>
       Experience the future of data management engage with Webdatanets and elevate your business to new heights! Your data, your rules, your success.</Typography>
-      <Box sx={{justifyContent: 'space-evenly'}}>
+      <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '25px', justifyContent: 'center'}}>
         {landings.filter(landing => landing.button).map((landing, index) =>
           <Cards index={index} icon={landing.icon} title={landing.title} text={landing.description} button={landing.button} />
         )}
-      </Box>
+      </div>
     </Box>
   )
 }
