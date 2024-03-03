886b74d46de47be6be8f3164da4bcef828f0bbad
Eric Muriel <ericmuriel@hotmail.com>
2024-03-03 18:16:32 +0100

    changing banner

--- src/pages/LandingPage/components/Header/index.tsx
+++ src/pages/LandingPage/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Button, Typography, useTheme } from '@mui/material';
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ButtonDas } from './../../../../components/ButtonDas/index';
@@ -6,20 +6,48 @@ import { GenericContext } from '../../../../context/GenericContext';
 
 export const Header = () => {
   const { t, i18n } = useContext(GenericContext);
+  const theme = useTheme();
+
   return (
     <Box>
       <ButtonDas text='EN' onClick={() => i18n?.changeLanguage('en')}/>
       <ButtonDas text='ES' onClick={() => i18n?.changeLanguage('es')}/>
-      <h2 style={{fontWeight:700}}>{t('header.title-1')}<span  style={{color:'#fe5b05', fontStyle:'bold', marginLeft:4}} >{t('header.title-2')}</span></h2>
-      <Box sx={{ mt:2, mb:5 }}>
-        <img height={25} width={256} src={`${process.env.REACT_APP_BACKEND_ENPDOINT}${"/media-files/imagenes/banner-line.png"}`}/>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        [theme.breakpoints.up('lg')]: {
+          flexDirection: 'row',
+        },
+      }}>
+        <Box sx={{ flex: '1', marginRight: 2 }}>
+          <h2 style={{ fontWeight: 700 }}>{t('header.title-1')}<span style={{ color: '#fe5b05', fontStyle: 'bold', marginLeft: 4 }}>{t('header.title-2')}</span></h2>
+          <Box sx={{ mt: 2, mb: 5 }}>
+            <img height={25} width={256} src={`${process.env.REACT_APP_BACKEND_ENPDOINT}${"/media-files/imagenes/banner-line.png"}`} alt="Banner Line" />
+          </Box>
+          <p style={{ fontWeight: 400 }}>{t("header.description")}</p>
+        </Box>
+
+        <Box sx={{ flex: '1',display: { xs: 'none', sm: 'none', md: 'none', lg: 'block' }, [theme.breakpoints.up('md')]: { display: 'flex' } }}>
+          <Box>
+            <img height={590} width={800} src={`${process.env.REACT_APP_BACKEND_ENPDOINT}${"/media-files/imagenes/landing-banner.png"}`} alt="Landing Banner" />
+          </Box>
+        </Box>
       </Box>
-      <p style={{fontWeight:400}}> {t("header.description")} </p>
-      <Box sx={{my:4}}>
-        <img height={200} width={256} src={`${process.env.REACT_APP_BACKEND_ENPDOINT}${"/media-files/imagenes/landing-banner.png"}`}/>
+
+      <Box sx={{
+        my: 4,
+        display: { xs: 'block', sm: 'block', md: 'none', lg: 'none' },
+      }}>
+        <img height={200} width={256} src={`${process.env.REACT_APP_BACKEND_ENPDOINT}${"/media-files/imagenes/landing-banner.png"}`} alt="Landing Banner" />
       </Box>
-      <Box sx={{ textAlign: 'center' }}>
-        <Button sx={{backgroundColor:'#fe5b05', color:'#FFFFFF', padding:2}}><span>Try Demo</span></Button>
+
+      <Box sx={{
+        textAlign: 'center',
+        [theme.breakpoints.up('lg')]: {
+          display: 'none',
+        },
+      }}>
+        <Button sx={{ backgroundColor: '#fe5b05', color: '#FFFFFF', padding: 2 }}><span>Try Demo</span></Button>
       </Box>
     </Box>
   );
