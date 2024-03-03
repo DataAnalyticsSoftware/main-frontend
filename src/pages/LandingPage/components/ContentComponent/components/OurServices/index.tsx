b3ab36f2e364e9c9df2d78b4f84b28cc8bdfa62b
Victor Ortega <victorortega0309@gmail.com>
2024-03-03 18:25:24 +0100

    Change message our services

--- src/pages/LandingPage/components/ContentComponent/components/OurServices/index.tsx
+++ src/pages/LandingPage/components/ContentComponent/components/OurServices/index.tsx
@@ -9,12 +9,14 @@ export const OurServices=()=> {
 
   return (
     <Box sx={{paddingTop:8}}>
-      <h4 style={{color:'#fe5b05', paddingBottom:2}}>OUR SERVICES</h4>
-      <h1 >The Best Platform For Your Business</h1>
-      <p style={{paddingTop:2}}>
-        We offer a verity of interesting features that you can help increase your productivity at work and manage your Business easily.
-      </p>
       <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: '10px', justifyContent: 'center', alignItems: 'center'}}>
+        <div style={{paddingBottom:'2px', textAlign: 'left', backgroundColor:'white',color:'black',marginBottom:'4px', width: '400px'}}>
+          <h4 style={{color:'#fe5b05', paddingBottom:2}}>OUR SERVICES</h4>
+          <h2>The Best Platform For Your Business</h2>
+          <p style={{paddingTop:2}}>
+            We offer a verity of interesting features that you can help increase your productivity at work and manage your Business easily.
+          </p>
+        </div>
         {landings.filter((landing) => !landing.button).map((landing, index) =>
           <Cards key={index} index={index} icon={landing.icon} title={landing.title} text={landing.description} button={landing.button} />
         )}
