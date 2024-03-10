8243d2e02d2a7a034ee1eb2c7a7a6fb4e722f81e
Victor Ortega <victorortega0309@gmail.com>
2024-03-10 20:17:23 +0100

    second cards transalte

--- src/utils/cards.tsx
+++ src/utils/cards.tsx
@@ -13,8 +13,8 @@ export const Cards=({icon,title,text,button, index, colorIcon = '', colorBackgro
         <i style={{fontSize: '48px', color: colorIcon}} className={icon}></i>
       </div>
       <div style={{backgroundColor: 'white', padding: '63px 15px', paddingBottom: '20px', color: 'black', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '230px', borderRadius:'10px'}}>
-        <p style={{marginTop:4, fontWeight:700}}>{title}</p>
-        <p style={{lineHeight:1.3,paddingBottom:2}}>{text}</p>
+        <p style={{marginTop:4, fontWeight:700}}>{t(`whyChooseUs.${title}`)}</p>
+        <p style={{lineHeight:1.3,paddingBottom:2}}>{t(`whyChooseUs.${text}`)}</p>
       </div>
     </div>
     </>
