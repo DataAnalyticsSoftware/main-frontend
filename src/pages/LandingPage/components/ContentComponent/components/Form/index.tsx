19f9966b5ff68ede51988e4077d7ab33a238846d
Victor Ortega <victorortega0309@gmail.com>
2024-04-14 23:31:09 +0200

    feat: change questions

--- src/pages/LandingPage/components/ContentComponent/components/Form/index.tsx
+++ src/pages/LandingPage/components/ContentComponent/components/Form/index.tsx
@@ -1,143 +1,252 @@
-import React, { useContext, useState } from 'react'
-import { GenericContext } from '../../../../../../context/GenericContext'
-import TextField from '@mui/material/TextField'
-import Radio from '@mui/material/Radio'
-import RadioGroup from '@mui/material/RadioGroup'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import { Button, MobileStepper } from '@mui/material'
-import styles from './styles.module.scss'
-import { questions } from './questions'
-import InfoIcon from '@mui/icons-material/Info'
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft'
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight'
-import { useNavigate } from "react-router-dom"
+import React, { useContext, useState } from "react";
+import { GenericContext } from "../../../../../../context/GenericContext";
+import TextField from "@mui/material/TextField";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { Button, MobileStepper } from "@mui/material";
+import styles from "./styles.module.scss";
+import { questions } from "./questions";
+import InfoIcon from "@mui/icons-material/Info";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import { useNavigate } from "react-router-dom";
 
-
-export const Form=()=> {
-  const navigate = useNavigate()
-  const [email, setEmail] = useState<string | null>(null)
-  const [comment, setComment] = useState<string | null>(null)
-  const [responses, setResponses] = useState<any[]>(Array(questions.length).fill(null))
-  const [formComplete, setFormComplete ] = useState<boolean>(Boolean(localStorage.getItem('submissionSuccessful')) || false)
-  const [activeStep, setActiveStep] = useState<number>(0)
-  const { t, webDataNetsRequest } = useContext(GenericContext)
+export const Form = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState<string | null>(null);
+  const [comment, setComment] = useState<string | null>(null);
+  const [responses, setResponses] = useState<any[]>(
+    Array(questions.length).fill(null)
+  );
+  const [formComplete, setFormComplete] = useState<boolean>(
+    Boolean(localStorage.getItem("submissionSuccessful")) || false
+  );
+  const [activeStep, setActiveStep] = useState<number>(0);
+  const { t, webDataNetsRequest } = useContext(GenericContext);
 
   const regexEmail =
-  /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-  const PERCENTAGE_OPTIONS : (string | null)[] =[null, null, '10%', '8%', '4.5%', '2.25%', '1.95%']
+  const PERCENTAGE_OPTIONS: (string | null)[] = [
+    null,
+    null,
+    "10%",
+    "8%",
+    "4.5%",
+    "2.25%",
+    "1.95%",
+  ];
 
-  const handleSubmit = () =>{
-    navigate('/RegisterScreen', { replace: false })
-    if(!email && responses.filter(x=>x).length === 0) return
+  const handleSubmit = () => {
+    navigate("/RegisterScreen", { replace: false });
     const data = {
-      email: email || 'unknown',
+      email: email || "unknown",
       comment: comment,
-      responses: responses
-    }
-    webDataNetsRequest('api/submit_form',JSON.stringify(data), 'POST').then(() =>{
-      localStorage.setItem('submissionSuccessful','true')
-      setFormComplete(true)
-    })
-  }
-console.log(activeStep);
+      responses: responses,
+    };
+    webDataNetsRequest("api/submit_form", JSON.stringify(data), "POST").then(
+      () => {
+        localStorage.setItem("submissionSuccessful", "true");
+        setFormComplete(true);
+      }
+    );
+  };
 
   const handleNext = () => {
-    if(activeStep === 1 && responses[activeStep-1] !== '0'){
-      let newResponses = [...responses]
-      newResponses[1] = null
-      newResponses[2] = null
-      setResponses(newResponses)
-      setActiveStep((prevActiveStep) => prevActiveStep + 3);
-    }else if(activeStep === 2 && (responses[activeStep-1] === '0' || responses[activeStep-1] === '1')){
-      let newResponses = [...responses]
-      newResponses[2] = null
-      setResponses(newResponses)
+    if (activeStep === 2 && Number(responses[1]) >= 2) {
+      questions.push({
+        question:
+          "¿Has tenido problemas con la gestión de datos de tus clientes?",
+        answers: ["Sí, a menudo", "A veces", "No, rara vez", "No, rara vez"],
+      });
+    } else if (activeStep === 2 && questions.length === 8) {
+      questions.pop();
+    }
+
+    if (activeStep === 1 && responses[activeStep - 1] !== "0") {
+      let newResponses = [...responses];
+      newResponses[1] = null;
+      setResponses(newResponses);
       setActiveStep((prevActiveStep) => prevActiveStep + 2);
-    }else{
+    } else if (activeStep === 6 && Number(responses[1]) >= 2) {
+      setActiveStep((prevActiveStep) => prevActiveStep + 2);
+    } else {
       setActiveStep((prevActiveStep) => prevActiveStep + 1);
     }
   };
 
   const handleBack = () => {
-    if(activeStep === 4 && responses[0] !== '0')
-      setActiveStep((prevActiveStep) => prevActiveStep - 3);
-    else if(activeStep === 4 && (responses[1] === '0' || responses[1] === '1'))
+    if (activeStep === 3 && responses[0] !== "0")
       setActiveStep((prevActiveStep) => prevActiveStep - 2);
-    else
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    else if (activeStep === 8 && Number(responses[1]) >= 2)
+      setActiveStep((prevActiveStep) => prevActiveStep - 2);
+    else setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  const handleResponse = (index: number, value: React.ChangeEvent<HTMLInputElement>) => {
-    const newResponses = [...responses]
-    newResponses[index] = value.target.value
-    setResponses(newResponses)
-  }
+  const handleResponse = (
+    index: number,
+    value: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const newResponses = [...responses];
+    newResponses[index] = value.target.value;
+    setResponses(newResponses);
+  };
 
   const comprobateStep = (actualStep: number) => {
-    if(!actualStep) return !!email && email.trim() !== '' && email.toLowerCase().match(regexEmail)
-    return !!responses[actualStep-1]
-  }
+    if (!actualStep)
+      return (
+        !!email && email.trim() !== "" && email.toLowerCase().match(regexEmail)
+      );
+    return !!responses[actualStep - 1];
+  };
 
   return (
     <>
-      <h1 style={{fontWeight:700, paddingTop:20, paddingBottom:20}} id='demo'>{t('get-demo.header')} <span style={{color:'#fe5b05'}}> {t('get-demo.subheader')}</span></h1>
-      {formComplete ? <div style={{ backgroundColor: '#F3F3F3', padding: '60px 10px', textAlign: 'center'}}>
-        {t("questions.final-message")}
-      </div> :
-      <div style={{ backgroundColor: '#F3F3F3', padding: '60px 40px', textAlign: 'left', borderRadius:10}}>
-        <div className={styles.formDas} style={{alignItems: 'center', display: 'flex', flexDirection: 'column', gap: '10px', textAlign: 'center'}}>
-        {activeStep === 0 && <div style={{display: 'flex', flexDirection: 'column'}}>
-          <TextField
-              label="Email"
-              id="outlined-start-adornment"
-              sx={{ m: 1, width: '100%' }}
-              defaultValue={email}
-              onChange={(event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => setEmail(event.target.value)}
-          />
-          <label style={{color: 'blue', fontSize: 15, textAlign:'center'}}><InfoIcon style={{fontSize: 15}} />{t('get-demo.title')}</label>
-        </div>}
-        {activeStep > 0 && <div>
-            <label id={`question${activeStep-1}`}>{t(`questions.${activeStep-1}`).replace('{percentage}',PERCENTAGE_OPTIONS[responses[1]] || '' )}</label>
-            <RadioGroup
-              sx={{justifyContent:'center'}}
-              row
-              aria-labelledby={`question${activeStep-1}`}
-              name={`question${activeStep-1}`}
-              value={responses[activeStep-1]}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => handleResponse(activeStep-1, event)}
-            >
-              {questions[activeStep-1].answers.map((answer: string, indexAnswer: number) =>
-                <FormControlLabel key={indexAnswer} value={indexAnswer} control={<Radio sx={{'&.Mui-checked':{color: '#FF5A05'}}} />} label={t(`posible-answers.${activeStep}-${indexAnswer+1}`)} />
-              )}
-            </RadioGroup>
-          </div>}
-      </div>
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', paddingTop: '20px'}}>
-        <MobileStepper
-          variant="progress"
-          steps={questions.length+1}
-          position="static"
-          activeStep={activeStep}
-          sx={{ maxWidth: 400, flexGrow: 1, bgcolor: '#F3F3F3' }}
-          nextButton={
-           <> {activeStep !== questions.length ? <Button size="small" onClick={handleNext} disabled={!comprobateStep(activeStep)}>
-              {t('questions.next')} <KeyboardArrowRight />
-            </Button> :
-            <Button size="small" onClick={()=>handleSubmit} disabled={!comprobateStep(activeStep)}>
-              {t('questions.finish')}
-            </Button>
-            }</>
-          }
-          backButton={
-            <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-              <KeyboardArrowLeft />
-              {t('questions.back')}
-            </Button>
-          }
-        />
-      </div>
-    </div>}
+      <h1
+        style={{ fontWeight: 700, paddingTop: 20, paddingBottom: 20 }}
+        id="demo"
+      >
+        {t("get-demo.header")}{" "}
+        <span style={{ color: "#fe5b05" }}> {t("get-demo.subheader")}</span>
+      </h1>
+      {formComplete ? (
+        <div
+          style={{
+            backgroundColor: "#F3F3F3",
+            padding: "60px 10px",
+            textAlign: "center",
+          }}
+        >
+          {t("questions.final-message")}
+        </div>
+      ) : (
+        <div
+          style={{
+            backgroundColor: "#F3F3F3",
+            padding: "60px 40px",
+            textAlign: "left",
+            borderRadius: 10,
+          }}
+        >
+          <div
+            className={styles.formDas}
+            style={{
+              alignItems: "center",
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              textAlign: "center",
+            }}
+          >
+            {activeStep === 0 && (
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <TextField
+                  label="Email"
+                  id="outlined-start-adornment"
+                  sx={{ m: 1, width: "100%" }}
+                  defaultValue={email}
+                  onChange={(
+                    event: React.ChangeEvent<
+                      HTMLInputElement | HTMLTextAreaElement
+                    >
+                  ) => setEmail(event.target.value)}
+                />
+                <label
+                  style={{ color: "blue", fontSize: 15, textAlign: "center" }}
+                >
+                  <InfoIcon style={{ fontSize: 15 }} />
+                  {t("get-demo.title")}
+                </label>
+              </div>
+            )}
+            {activeStep > 0 && (
+              <div>
+                <label id={`question${activeStep - 1}`}>
+                  {t(`questions.${activeStep - 1}`).replace(
+                    "{percentage}",
+                    PERCENTAGE_OPTIONS[responses[1]] || ""
+                  )}
+                </label>
+                <RadioGroup
+                  sx={{ justifyContent: "center" }}
+                  row
+                  aria-labelledby={`question${activeStep - 1}`}
+                  name={`question${activeStep - 1}`}
+                  value={responses[activeStep - 1]}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    handleResponse(activeStep - 1, event)
+                  }
+                >
+                  {questions[activeStep - 1].answers.map(
+                    (answer: string, indexAnswer: number) => (
+                      <FormControlLabel
+                        key={indexAnswer}
+                        value={indexAnswer}
+                        control={
+                          <Radio
+                            sx={{ "&.Mui-checked": { color: "#FF5A05" } }}
+                          />
+                        }
+                        label={t(
+                          `posible-answers.${activeStep}-${indexAnswer + 1}`
+                        )}
+                      />
+                    )
+                  )}
+                </RadioGroup>
+              </div>
+            )}
+          </div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              paddingTop: "20px",
+            }}
+          >
+            <MobileStepper
+              variant="progress"
+              steps={questions.length + 1}
+              position="static"
+              activeStep={activeStep}
+              sx={{ maxWidth: 400, flexGrow: 1, bgcolor: "#F3F3F3" }}
+              nextButton={
+                <>
+                  {activeStep !== questions.length ? (
+                    <Button
+                      size="small"
+                      onClick={handleNext}
+                      disabled={!comprobateStep(activeStep)}
+                    >
+                      {t("questions.next")} <KeyboardArrowRight />
+                    </Button>
+                  ) : (
+                    <Button
+                      size="small"
+                      onClick={() => handleSubmit()}
+                      disabled={!comprobateStep(activeStep)}
+                    >
+                      {t("questions.finish")}
+                    </Button>
+                  )}
+                </>
+              }
+              backButton={
+                <Button
+                  size="small"
+                  onClick={handleBack}
+                  disabled={activeStep === 0}
+                >
+                  <KeyboardArrowLeft />
+                  {t("questions.back")}
+                </Button>
+              }
+            />
+          </div>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
--- src/pages/LandingPage/components/ContentComponent/components/Form/questions.ts
+++ src/pages/LandingPage/components/ContentComponent/components/Form/questions.ts
@@ -1,53 +1,58 @@
-
 export interface IQuestions {
-    question: string
-    answers: string[]
+  question: string;
+  answers: string[];
 }
 
 export const questions: IQuestions[] = [
-    {
-        question: "¿Has tenido problemas con la gestión de datos de tus clientes?",
-        answers: [
-            'Sí, a menudo','A veces','No, rara vez'
-        ]
-    },{
-        question: "¿Has tenido problemas con la gestión de datos de tus clientes?",
-        answers: [
-            'Sí, a menudo','A veces','No, rara vez','No, rara vez','No, rara vez','No, rara vez'
-        ]
-    },{
-        question: "¿Has tenido problemas con la gestión de datos de tus clientes?",
-        answers: [
-            'Sí, a menudo','A veces','No, rara vez','No, rara vez'
-        ]
-    },{
-        question: "¿Has tenido problemas con la gestión de datos de tus clientes?",
-        answers: [
-            'Sí, a menudo','A veces','No, rara vez'
-        ]
-    },
-    {
-        question: "Desafíos actuales en la gestión de datos y clientes:",
-        answers: [
-            'Organización, Filtrado, Modificación de datos.','Seguimiento de interacciones.','Desconozco el número de todos mis clientes.'
-        ]
-    },
-    {
-        question: "¿Buscas una solución para gestionar la información de clientes y tus bases de datos?",
-        answers: [
-            'Sí, estoy buscando activamente.','No, pero podría considerarlo.','No, no necesito ninguna solución.'
-        ]
-    },
-    {
-        question: "¿Conoces el potencial de la información de tus clientes y tus bases de datos?",
-        answers: [
-            'Sí, estoy al tanto.','No estoy seguro.','No, explícame más.'
-        ]
-    },
-    {
-        question: "¿Estarías dispuesto a pagar por una solución? ¿Cuánto?",
-        answers: [
-            'Si, hasta 30€.','Sí, entre 50€ y 100€ al mes.','Sí, pero no más de 200€ al mes.', 'No estaría dispuesto a pagar.'
-        ]
-    }
-]
+  {
+    question: "¿Has tenido problemas con la gestión de datos de tus clientes?",
+    answers: ["Sí, a menudo", "A veces", "No, rara vez"],
+  },
+  {
+    question: "¿Has tenido problemas con la gestión de datos de tus clientes?",
+    answers: [
+      "Sí, a menudo",
+      "A veces",
+      "No, rara vez",
+      "No, rara vez",
+      "No, rara vez",
+      "No, rara vez",
+      "No, rara vez",
+    ],
+  },
+  {
+    question: "¿Has tenido problemas con la gestión de datos de tus clientes?",
+    answers: ["Sí, a menudo", "A veces", "No, rara vez"],
+  },
+  {
+    question: "Desafíos actuales en la gestión de datos y clientes:",
+    answers: [
+      "Organización, Filtrado, Modificación de datos.",
+      "Seguimiento de interacciones.",
+      "Desconozco el número de todos mis clientes.",
+    ],
+  },
+  {
+    question:
+      "¿Buscas una solución para gestionar la información de clientes y tus bases de datos?",
+    answers: [
+      "Sí, estoy buscando activamente.",
+      "No, pero podría considerarlo.",
+      "No, no necesito ninguna solución.",
+    ],
+  },
+  {
+    question:
+      "¿Conoces el potencial de la información de tus clientes y tus bases de datos?",
+    answers: ["Sí, estoy al tanto.", "No estoy seguro.", "No, explícame más."],
+  },
+  {
+    question: "¿Estarías dispuesto a pagar por una solución? ¿Cuánto?",
+    answers: [
+      "Si, hasta 30€.",
+      "Sí, entre 50€ y 100€ al mes.",
+      "Sí, pero no más de 200€ al mes.",
+      "No estaría dispuesto a pagar.",
+    ],
+  },
+];
